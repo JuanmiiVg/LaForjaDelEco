0c909661fc0587150920e519a4219e01dcdd7044
Francisco <franciscojosesanchezlloret@gmail.com>
2025-02-25 20:15:29 +0100

    25/02 como lo de juanma pero con mis rutas

--- LaForjaDelEco/routes/web.php
+++ LaForjaDelEco/routes/web.php
@@ -2,10 +2,23 @@
 
 use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\inventarioControler;
+use App\Http\Controllers\personajeController;
+use App\Models\inventario;
+use App\Models\personaje;
+
 
 Route::get('/', function () {
     return view('welcome');
 });
+
+//Ruta que muestra el inventario del personaje
+Route::get("/personaje/{id}",[personajeController::class,"index"])->name("inventario");
+//Rutas para borrar
+Route::delete("personaje/{id}/eliminar/{idArm}",[personajeController::class,"deleteArma"])->name("arma.eliminar");
+Route::delete("personaje/{id}/eliminar/{idPoc}",[personajeController::class,"deletePocion"])->name("pocion.eliminar");
+Route::delete("personaje/{id}/eliminar/{idMat}",[personajeController::class,"deleteMaterial"])->name("material.eliminar");
+Route::delete("personaje/{id}/eliminar/{idIng}",[personajeController::class,"deleteIngrediente"])->name("ingrediente.eliminar");
 
 Route::get('/dashboard', function () {
     return view('dashboard');
