4b7c2ce70f9e88168bf5464f53ab5a9f9cf64a24
JuanmiiVg <146075100+JuanmiiVg@users.noreply.github.com>
2025-02-25 20:04:07 +0100

    25/02 Instalacion Breeze y Dependencias (Socialitee instalado pero no falta hacer el codigo)

--- LaForjaDelEco/routes/web.php
+++ LaForjaDelEco/routes/web.php
@@ -1,18 +1,20 @@
 <?php
 
-use App\Http\Controllers\inventarioControler;
-use App\Http\Controllers\personajeController;
-use App\Models\inventario;
-use App\Models\personaje;
+use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
 });
-//Ruta que muestra el inventario del personaje
-Route::get("/personaje/{id}",[personajeController::class,"index"])->name("inventario");
-//Rutas para borrar
-Route::delete("personaje/{id}/eliminar/{idArm}",[personajeController::class,"deleteArma"])->name("arma.eliminar");
-Route::delete("personaje/{id}/eliminar/{idPoc}",[personajeController::class,"deletePocion"])->name("pocion.eliminar");
-Route::delete("personaje/{id}/eliminar/{idMat}",[personajeController::class,"deleteMaterial"])->name("material.eliminar");
-Route::delete("personaje/{id}/eliminar/{idIng}",[personajeController::class,"deleteIngrediente"])->name("ingrediente.eliminar");
+
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->middleware(['auth', 'verified'])->name('dashboard');
+
+Route::middleware('auth')->group(function () {
+    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+});
+
+require __DIR__.'/auth.php';
